6b2979229cb517f3db1c04c829088fdedffd0a88
Alexander Ignot <91916547+AlexIgnot@users.noreply.github.com>
2023-02-10 00:25:11 +0000

    Hello world en C++

---
+++ Parcial 1/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello world" << endl;
+    cout << "Hello galaxy" << endl;
+    cout << "Hello universe" << endl;
+    return 0;
+
+    }
