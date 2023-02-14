3b15bcbbd1e19c09b632f626f4f2649f192fb8e7
Alexander Ignot <91916547+AlexIgnot@users.noreply.github.com>
2023-02-14 21:25:26 +0000

    Renombré archivos y añadí uno nuevo

---
+++ parcial1/helloWorld2.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	std::cout << "Hello world 2" << endl;
+	std::cout << "Vi es confuso" << endl;
+	std::cout << "ALEXIGNOT" << endl;
+	std::cout << "wichas" << endl;
+
+	return 0;
+	
+	} 
