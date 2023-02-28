4f24ee7145c797d9080b03b9984af6faf7d8c4f0
Alexander Ignot <91916547+AlexIgnot@users.noreply.github.com>
2023-02-28 22:32:57 +0000

    Ejercicio threshold

--- parcial1/threshold.cpp
+++ parcial1/threshold.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <math>
+#include <cmath>
 
 using namespace std;
 
@@ -13,8 +13,8 @@ int H (double* w, double* x, int n)
 int sign(double* w, double* x, int n)
 {
 	double sum = 0.0;
-	for(int = 0; i <= n; i++) { sum += w[i]*x[i]; }
-	if(sum >= 0.0) return 1;
+	for(int i= 0; i <= n; i++) { sum += w[i]*x[i]; }
+	if(sum > 0.0) return 1;
 	else return 0;
 }
 
@@ -33,32 +33,36 @@ double bipolar(double* w, double* x, int n)
 	for(int i = 0; i <= n; i++) { sum += w[i]*x[i]; }
 	return tanh(lambda*sum);
 }
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-} 
 
+int main(void)
+{
+	int n = 5; //length of input vector includes bias
+	double theta = 0.5;	//threshold
+
+	//memory allocation for weight vector w
+	double* w = new double[n];
+	w[0] = -theta;
+	w[1] = 0.7; w[2] = -1.1; w[3] = 4.5; w[4] = 1.5;
+
+	//memory allocation for input vector x
+	double* x = new double[n];
+	x[0] = 1.0; // bias
+	x[1] = 0.7;	x[2] = 1.2; x[3] = 1.5; x[4] = -4.5;
+
+	int r1 = H(w, x, n);
+	cout << "r1 = " << r1 << endl;
+
+	int r2 = sign(w, x, n);
+	cout << "r2 = " << r2 << endl;
+
+	double r3 = unipolar(w, x, n);
+	cout << "r3 = " << r3 << endl;
+
+	double r4 = bipolar(w, x, n);
+	cout << "r4 = " << r4 << endl;
+
+	delete [] w; delete [] x;
+	return 0;	
+} 
 
 
